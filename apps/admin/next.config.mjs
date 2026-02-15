31170a048784d1929ac614cd875d2a5948fb6223
sebastian <sebastian@apiflujos.com>
2026-02-14 19:23:19 -0500

    chore: ignore eslint during build

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -3,7 +3,8 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  outputFileTracingRoot: path.join(process.cwd(), "../..")
+  outputFileTracingRoot: path.join(process.cwd(), "../.."),
+  eslint: { ignoreDuringBuilds: true }
 };
 
 export default nextConfig;
