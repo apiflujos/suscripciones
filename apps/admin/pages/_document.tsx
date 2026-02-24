e42dd97f40e8f60208f806d042abb4fdf75748fa
Apiflujos S.A.S <comercial@apiflujos.com>
2026-02-24 16:48:29 -0500

    Admin Document: call getInitialProps

--- apps/admin/pages/_document.tsx
+++ apps/admin/pages/_document.tsx
@@ -2,6 +2,11 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="es">
