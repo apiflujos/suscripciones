c1d1b88a26dbdfb0dcffb45585f18416006d7e51
Apiflujos S.A.S <comercial@apiflujos.com>
2026-02-24 14:27:27 -0500

    fix(admin): restore Html in document

--- apps/admin/pages/_document.js
+++ apps/admin/pages/_document.js
@@ -1,15 +1,15 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <html lang="es">
+      <Html lang="es">
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
