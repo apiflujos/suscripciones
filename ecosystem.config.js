1a5aa7e69c983b882fb8a402aaf93a23629689ab
sebastian giraldo <segimu7@gmail.com>
2026-02-25 08:06:49 -0500

    add pm2 process for admin

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,6 +29,22 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '300M'
+    },
+    {
+      name: 'wompi-subs-admin',
+      cwd: './apps/admin',
+      script: 'npm',
+      args: 'run start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      error_file: '../../logs/admin-error.log',
+      out_file: '../../logs/admin-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M'
     }
   ]
 };
