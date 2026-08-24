9d409823461641bc4a4dc05ec1a58f2bd4ca2611
sebastian giraldo <sebastiangiraldo@MacBook-Air-de-sebastian.local>
2026-08-24 15:48:13 -0500

    Remove accidentally committed .js build artifacts

--- packages/core/src/services/urlSafety.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.normalizeRenderablePublicUrl = normalizeRenderablePublicUrl;
-const publicBase_1 = require("./publicBase");
-function normalizeRenderablePublicUrl(raw) {
-    return (0, publicBase_1.normalizePublicUrl)(raw, { allowLocalhost: false });
-}
